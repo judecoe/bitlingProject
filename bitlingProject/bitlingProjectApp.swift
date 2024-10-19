519dbab6f3262c73a32d216c6ea17dfa5b0e3da4
judecoe <judegilligan@gmail.com>
2024-10-19 14:46:36 -0400

    Initial Commit

---
+++ bitlingProject/bitlingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitlingProjectApp.swift
+//  bitlingProject
+//
+//  Created by Jude Gilligan on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct bitlingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
